564a24e7a3605a9cf39a6f3f900b2fcb32aa013e
laurens <laurens.inauen@hkb.bfh.ch>
2021-03-23 23:28:32 +0100

    push slides

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 reveal-md index.md --css assets/style.css --static docs  --static-dirs=assets
 
-# replaces _assets with assets in the css link because github won't recognize it
-sed -i 's/_assets/dist/' docs/index.html
+# replaces _assets/assets with assets in the css link because github won't recognize it
+sed -i 's/_assets\/assets/assets/' docs/index.html
